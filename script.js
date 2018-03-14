eb0d53189271ec836673cc5c63e73ccb708fa3ae
Codenation2 <Codenation2@CodeNations-MacBook-Pro-2.local>
2018-03-14 14:20:33 +0000

    victory function
    added function

---
+++ script.js
@@ -0,0 +1,252 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function victory()  {
+   if (arr[1] == 1 && arr[2] == 1 && arr[3] == 1) 
+   { alert ("Crosses won")
+} 
+else if (arr[1] == 2 && arr [2]==2 && arr[3] ==2) {
+    alert ("Noughts won")
+}
+else if (arr[4] == 1 && arr [5]==1 && arr[6] ==2) {
+    alert ("Crosses won")
+}
+else if (arr[4] == 2 && arr [5]==2 && arr[6] ==2) {
+    alert ("Noughts won")
+}
+else if (arr[7] == 1 && arr [8]==1 && arr[9] ==1) {
+    alert ("Crosses won")
+}
+else if (arr[7] == 2 && arr [8]==2 && arr[9] ==2) {
+    alert ("Noughts won")
+}
+else if (arr[1] == 1 && arr [5]==1 && arr[9] ==1) {
+    alert ("Crosses won")
+}
+else if (arr[1] == 2 && arr [5]==2 && arr[9] ==2) {
+    alert ("Noughts won")
+}
+else if (arr[7] == 1 && arr [5]==1 && arr[3] ==1) {
+    alert ("Crosses won")
+}
+
+else if (arr[7] == 2 && arr [5]==2 && arr[3] ==2) {
+    alert ("Noughts won")
+}
+else if (arr[1] == 1 && arr [4]==1 && arr[7] ==1) {
+    alert ("Crosses won")
+}
+else if (arr[1] == 2 && arr [4]==2 && arr[7] ==2) {
+    alert ("Noughts won")
+}
+else if (arr[2] == 1 && arr [5]==1 && arr[8] ==1) {
+    alert ("Crosses won")
+}
+else if (arr[2] == 2 && arr [5]==2 && arr[8] ==2) {
+    alert ("Noughts won")
+}
+else if (arr[3] == 1 && arr [6]==1 && arr[9] ==1) {
+    alert ("Crosses won")
+}
+else if (arr[3] == 2 && arr [6]==2 && arr[9] ==2) {
+    alert ("Noughts won")
+}
+
+}
